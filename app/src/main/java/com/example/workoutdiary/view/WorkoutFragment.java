001b5d7629cd7adf74e16581a4ec811486415f65
Sergey Ozernyy <s.ozerny@avsw.ru>
2021-10-27 19:04:23 +0300

    Добавил граф навигации

--- app/src/main/java/com/example/workoutdiary/view/WorkoutFragment.java
+++ app/src/main/java/com/example/workoutdiary/view/WorkoutFragment.java
@@ -13,8 +13,7 @@ public class WorkoutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_workout, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_workout, container, false);
     }
 
 }
